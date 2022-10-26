e34304fe2e2267a338e8eae896d7695f69ddc5f5
ghadaG2022 <git.ghada@gmail.com>
2022-10-26 20:49:32 +0200

    2 commit

--- src/view/Dialog2.java
+++ src/view/Dialog2.java
@@ -1,6 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+
+ 
 
 package view;
 
@@ -10,9 +9,7 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-/**
- *
- * @author Administrator
+
 
 public class Dialog2 extends JDialog {
         private JLabel ItemName;
@@ -25,24 +22,34 @@ public class Dialog2 extends JDialog {
      private JTextField ItempriceText;
     
     public Dialog2(NewJFrame j) {
+
        ItemName= new JLabel("ItemName");
         ItemNameText = new JTextField(15);
+
+        Itemprice = new JLabel("Itemprice");
+        ItempriceText = new JTextField(15);
+
         ItemQuantity = new JLabel("ItemQuantity");
         ItemquantityText = new JTextField(15);
-        Itemprice = new JLabel("Itemprice");
-      ItempriceText = new JTextField(15);
+
+
+
          setLayout(new GridLayout(4, 4));
         ok= new JButton("OK");
-        cancel = new JButton("Cancel");
         ok.setActionCommand("inlineok");
+
+        cancel = new JButton("Cancel");
         cancel.setActionCommand("cancelInvoice");
+
         ok.addActionListener(j.getController());
         cancel.addActionListener(j.getController());
      
         add( ItemName);
         add(  ItemNameText);
+
         add(  ItemQuantity);
         add( ItemquantityText);
+
         add( Itemprice);
         add( ItemquantityText);
          add(ItempriceText);
@@ -54,15 +61,17 @@ public class Dialog2 extends JDialog {
         
         
     }
+
+    public JTextField getItemPrice() {
+        return ItempriceText;
+    }
     
      public JTextField getItemName() {
         return ItemNameText;
     }
     
 
-    public JTextField getItemPrice() {
-        return ItempriceText;
-    }
+
     
      public JTextField getItemQuantity() {
         return ItemquantityText;
@@ -70,4 +79,4 @@ public class Dialog2 extends JDialog {
     
 }
  
- */
+
